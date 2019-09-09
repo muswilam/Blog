fc0b1c5d51f3e0ffd5ed5b001eabd120e4cae3b5
Sully <jackswilam@gmail.com>
2019-09-09 20:25:39 +0200

    Using jquery for adding class and remove it to make input-error red

--- Blog/Models/Comment.cs
+++ Blog/Models/Comment.cs
@@ -11,13 +11,13 @@ namespace Blog.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Name's Required.") , MaxLength(128)]
+        [Required(ErrorMessage = "Name Required.") , MaxLength(128)]
         public string Name { get; set; }
 
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Invalid Email.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Comment is Required.")]
+        [Required(ErrorMessage = "Comment Required.")]
         public string Body { get; set; }
 
         public DateTime Time { get; set; }
